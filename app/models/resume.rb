8f57ee17ff79d0bdecd898c18b66767a4e292592
Yuwei Xu <yuwei_xu@outlook.com>
2015-03-07 02:11:34 -0500

    added tags on the resumes

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,2 +1,7 @@
 class Resume < ActiveRecord::Base
+
+	#list of companies that this resume received interview from
+	#list of faculties and majors
+	acts_as_taggable_on :companies, :faculties, :majors
+
 end
