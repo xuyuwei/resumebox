46a22097ed04e4783166bfc682db3fd89aee6f35
Ioana Crant <ioana.crant@gmail.com>
2015-03-09 01:01:35 -0400

    made user model with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Rails.application.routes.draw do
-  get 'resumes_controller/new'
+  
 
-  get 'resumes_contrller/new'
 
-  get 'resumes/new'
 
-  get 'resume/new'
-
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'staticpages#home'
+  root           'staticpages#home'
   get 'about' => 'staticpages#about'
+  get 'signup' =>'users#new'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
