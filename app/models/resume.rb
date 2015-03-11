217531a40b3b981ba49215e83100aa69591e4be6
Ioana Crant <ioana.crant@gmail.com>
2015-03-11 01:01:07 -0400

    Added resume URL validation and resume deletion

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -3,5 +3,6 @@ class Resume < ActiveRecord::Base
 	#list of companies that this resume received interview from
 	#list of faculties and majors
 	acts_as_taggable_on :companies, :faculties, :majors
+	validates :url, presence: true
 
 end
