d7abb97fe29b1f5a5eb7595db1bc13482b370fc1
Yuwei Xu <yuwei_xu@outlook.com>
2015-03-23 02:56:05 -0400

    yay for working uploads

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -3,9 +3,12 @@ class Resume < ActiveRecord::Base
 	#list of companies that this resume received interview from
 	#list of faculties and majors
 	acts_as_taggable_on :companies, :majors
-	validates :url, presence: true
 	validates :user_id, presence: true
-	has_attached_file :resume
+	has_attached_file :resume, 
+		:styles => {
+			fullsize: {geometry: "100%", format: :png,:processors => [:pdfprocessor]},
+	 		thumbnail: {geometry: "100%", format: :png,:processors => [:pdfprocessor_small]}
+	 	}
 	validates_attachment :resume, :content_type => { :content_type => %w(application/pdf application/msword) }
-
+		
 end
---
+++ lib/paperclip_processors/pdfprocessor.rb
@@ -0,0 +1,36 @@
+module Paperclip
+  class Pdfprocessor < Processor
+ 
+    attr_accessor :current_geometry, :target_geometry, :format, :whiny, :convert_options, :source_file_options
+ 
+    def initialize file, options = {}, attachment = nil
+      super
+      @file                = file
+      @format              = options[:format]
+ 
+      @current_format      = File.extname(@file.path)
+      @basename            = File.basename(@file.path, @current_format)
+    end
+ 
+    def make
+      src = @file
+      dst = Tempfile.new([@basename, @format ? ".#{@format}" : ''])
+      dst.binmode
+ 
+      begin
+        parameters = []
+        parameters << "-density 300"
+        parameters << ":source"
+        parameters << "-quality 100"
+        parameters << ":dest"
+ 
+        parameters = parameters.flatten.compact.join(" ").strip.squeeze(" ")
+        
+        success = Paperclip.run("convert", parameters, :source => "#{File.expand_path(src.path)}", :dest => File.expand_path(dst.path))
+      rescue PaperclipCommandLineError => e
+        raise PaperclipError, "There was an error processing the thumbnail for #{@basename}" if @whiny
+      end
+      dst
+    end
+  end
+end
