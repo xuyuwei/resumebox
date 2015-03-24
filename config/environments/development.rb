15ba9f04e9240dd07d3618c6485fdef323acd8ab
Yuwei Xu <yuwei_xu@outlook.com>
2015-03-24 00:16:35 -0400

    fixed bucket key stuff

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
     :storage => :s3,
     :s3_protocol => 'http',
     :s3_credentials => {
-      :bucket => 'resumebox',
+      :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV["AWS_ACCESS_KEY"],
       :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]
     },
