d3f3243075cb20eb237176bc8c8159515d62132a
Yuwei Xu <yuwei_xu@outlook.com>
2015-03-23 23:59:30 -0400

    resume-helper change

--- app/helpers/resumes_helper.rb
+++ app/helpers/resumes_helper.rb
@@ -4,7 +4,7 @@ module ResumesHelper
 			:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
 			:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
 		)
-		object = @s3.buckets['resumebox'].objects['resumes/resumes/000/000/'+ "%03d" % id+ '/fullsize/'+filename[0..-4]+"png"]
+		object = @s3.buckets[ENV['AWS_BUCKET']].objects['resumes/resumes/000/000/'+ "%03d" % id+ '/fullsize/'+filename[0..-4]+"png"]
 		@image_url = object.url_for(:get, {:expires => 30.minutes.from_now, :secure => true}).to_s;
 		return @image_url
 	end
@@ -14,7 +14,7 @@ module ResumesHelper
 			:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
 			:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
 		)
-		object = @s3.buckets['resumebox'].objects['resumes/resumes/000/000/'+ "%03d" % id+ '/thumbnail/'+filename[0..-4]+"png"]
+		object = @s3.buckets[ENV['AWS_BUCKET']].objects['resumes/resumes/000/000/'+ "%03d" % id+ '/thumbnail/'+filename[0..-4]+"png"]
 		@image_url = object.url_for(:get, {:expires => 30.minutes.from_now, :secure => true}).to_s;
 		return @image_url
 	end
